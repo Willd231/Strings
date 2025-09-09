9322315f6f3d25c482da55f258d4c724756c2304
Willd231 <dellinger.w3@gmail.com>
2025-09-09 13:49:46 -0400

    Finished Day One finally :( now i start dev on the day 2 set

--- src/days/1/step-1.c
+++ src/days/1/step-1.c
@@ -1,3 +1,4 @@
+
 /*
 William Dellinger,
 Monday - September 8th 2025 
@@ -8,8 +9,8 @@ Functions I plan to implement today:
 
  1.strlen - returns the length of a string in bytes before the terminating char ✅
  2.strcpy - copies a string to a destination this includes the terminating char ✅
- 3.strncpy - copies up to n characters from source to dest and uses term char if source is shorter but can omit if source is longer  
- 4.strcat - concatenates/appends source string to end of destination string overwriting its /0 
+ 3.strncpy - copies up to n characters from source to dest and uses term char if source is shorter but can omit if source is longer ✅  
+ 4.strcat - concatenates/appends source string to end of destination string overwriting its /0  ✅
  5.strncat - concatenates/appends up to n characters onto the end of a dest string and always adds the terminating character
 */
 
@@ -25,7 +26,7 @@ int not_strlen(char str[]){
 	}
 	
 	int count = 0;
-	
+
 	while(str[count] != '\0'){
 		count+=1; 
 	}
@@ -60,7 +61,7 @@ void not_strcpy(char dest[], char src[]){
 	
 	else if(diff <= -1){
 		
-
+	printf("error, buffer overflow");
 	return;
 
 
@@ -80,23 +81,46 @@ void not_strncpy(int n, char dest[], char src[]){
 	int dest_len = not_strlen(dest);
 	int diff = dest_len - src_len; 
 
+	/* Conditions brainstorm : 
+	s = 5 d = 5 n = 5 --> copy over whole source
+	s = 5 d = 5 n = 4 --> copy over 4/5 chars of the source fill the last value with the null char 
+	
+	
+	*/
+
+
+
+
 	//perfect condition - strings are equal length and n is equal to the lengths 
-	if(diff == 0 && src_len == n){
+	if(diff == 0){
+		if(src_len == n){
 		for(int i = 0; i < n; i++){
 			dest[i] = src[i]; 
+									}
+									
+									}
+
+		
+		else if(n > src_len){
+			printf("buffer overflow:");
+			return;
 		}
+		
 	}
 
-	else if(diff >= 1){
-		if(n <= src_len){	
-			for(int i =0; i < n;){
-				if(src[i + 1] == '\0' ){
-
-
-					
+	
+	else if(diff >= 1){ //if the length of the destination string is larger than the source
+	
+			
+			for(int i =0; i < dest_len;i++){
+				if(src[i] == '\0' ){
+					for(int j = 0; j < i - n; j ++){
+						dest[j] = '\0';
+					}
+					dest[i] = src[i]; 
 				}
 			}
-	}
+	
 	}
 }
 
@@ -108,8 +132,67 @@ void not_strncpy(int n, char dest[], char src[]){
 
 
 
+char * not_strcat(char source[], char destination[]){
+
+	int src_len = not_strlen(source);
+	int dest_len = not_strlen(destination);
+
+	int total = dest_len +  src_len + 2; 
+
+	
+	char * result = malloc(sizeof(char) * total + 1); 
+
+	for(int i = 0; i < total; i++){
+		if(i < dest_len){
+			result[i] = destination[i];
+		}
+		else if(i == total - 1){
+			result[i] = '\0';
+		}
+		else{
+			result[i] = source[i - dest_len];
+		}
+	}
+
+return result;
 
 
+
+
+}
+
+
+
+
+
+
+
+
+//adds n chars to the dest string and always adds term char
+char * not_strncat(char source[], char destination[], int n){
+
+	int total = not_strlen(destination) + n + 1;
+	char * result = malloc(sizeof(char) * total);
+	
+	
+		
+	
+	for(int i = 0; i < total; i++){
+		if(i < not_strlen(destination)){
+			result[i] = destination[i]; 
+		}
+		else if(i >= not_strlen(destination)){
+				result[i] = source[i];
+			
+			}
+		
+			
+
+		}
+	result[total - 1] = '\0'; 
+
+	
+}
 
 
 
@@ -123,9 +206,12 @@ int main(int argc, char * argv[]){
 		printf("Usage: 3 args");
 		return -1;
 	}
-	not_strcpy(argv[1], argv[2]);
+	//not_strcpy(argv[1], argv[2]);
 
-	printf("%s", argv[1]);
+
+	argv[1] = not_strcat(argv[1], argv[2]);
+	
+	printf("%s\n", argv[1]);
 	
 
 	
