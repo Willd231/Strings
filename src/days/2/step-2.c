caa217b3f8012aa962f7ba21bdfdeed7cee3162c
Willd231 <dellinger.w3@gmail.com>
2025-09-09 14:27:27 -0400

    organizing

--- src/days/2/step-2.c
+++ src/days/2/step-2.c
@@ -1,45 +1,51 @@
-/* William Dellinger,
+/* William Dellinger
 	September 9th 2025
 
-Functions I plan on implementing this day: 
+Functions I plan to implement today:
 
-	1. memset  -- usage =  void *memset(void *ptr, int value, size_t num);
-	2. memcpy
-	3. memcmp
-	4. memmove
-	*/
+	1. strcmp
+	2. strncmp 
+	3. strcoll 
+	4. strxfrm
+*/
 
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int not_strcmp(const char * str1, const char * str2){ //negative if s1 < s2 positive if s2 < s1 0 if equal 
 
+	len_1 = strlen(str1);
+	len_2 = strlen(str2);
+	
+	
+	int difference = str1[0] - str2[0];
 
-
-
-
-void * not_memset(void * ptr, int value, size_t num){
-
-
-
-
+	return difference;
 
 	
 }
 
 
+int not_strncmp(const char * str1, const char * str2, int n){
+
+	
+}
+
+int not_strcoll(const char * str1, const char * str2){
+	
+}
 
 
+int main(int argc, char * argv[]){
 
-
-
-
-
-
-
-int main (int argc, char * argv[]){
-
-
-
+	if(argc != 3){
+		printf("error: usage");
+		return -1;
+	}
+	
+	int result = not_strcmp(argv[1], argv[2]);
+	printf("%d", result);
 	return 0;
 }
+
---
+++ src/days/3/step-3.c
@@ -0,0 +1,45 @@
+/* William Dellinger,
+	September 9th 2025
+
+Functions I plan on implementing this day: 
+
+	1. memset  -- usage =  void *memset(void *ptr, int value, size_t num);
+	2. memcpy
+	3. memcmp
+	4. memmove
+	*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+void * not_memset(void * ptr, int value, size_t num){
+
+
+
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+int main (int argc, char * argv[]){
+
+
+
+	return 0;
+}
